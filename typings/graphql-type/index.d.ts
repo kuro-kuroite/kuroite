10238ce09508ef9c988ddda310a16eaecc03c453
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2021-05-19 11:46:42 +0000

    docs(create-firebase-user-task-application): :book: add content

--- typings/graphql-type/index.d.ts
+++ typings/graphql-type/index.d.ts
@@ -2947,11 +2947,6 @@ type SitePageFieldsEnum =
   | 'pluginCreator.pluginOptions.plugins.browserAPIs'
   | 'pluginCreator.pluginOptions.plugins.ssrAPIs'
   | 'pluginCreator.pluginOptions.plugins.pluginFilepath'
-  | 'pluginCreator.pluginOptions.gtagConfig.anonymize_ip'
-  | 'pluginCreator.pluginOptions.gtagConfig.cookie_expires'
-  | 'pluginCreator.pluginOptions.pluginConfig.head'
-  | 'pluginCreator.pluginOptions.pluginConfig.respectDNT'
-  | 'pluginCreator.pluginOptions.trackingIds'
   | 'pluginCreator.pluginOptions.outputPath'
   | 'pluginCreator.pluginOptions.showSpinner'
   | 'pluginCreator.pluginOptions.name'
@@ -2989,6 +2984,11 @@ type SitePageFieldsEnum =
   | 'pluginCreator.pluginOptions.allExtensions'
   | 'pluginCreator.pluginOptions.isTSX'
   | 'pluginCreator.pluginOptions.jsxPragma'
+  | 'pluginCreator.pluginOptions.gtagConfig.anonymize_ip'
+  | 'pluginCreator.pluginOptions.gtagConfig.cookie_expires'
+  | 'pluginCreator.pluginOptions.pluginConfig.head'
+  | 'pluginCreator.pluginOptions.pluginConfig.respectDNT'
+  | 'pluginCreator.pluginOptions.trackingIds'
   | 'pluginCreator.nodeAPIs'
   | 'pluginCreator.browserAPIs'
   | 'pluginCreator.ssrAPIs'
@@ -3191,11 +3191,6 @@ type SitePluginFieldsEnum =
   | 'pluginOptions.plugins.browserAPIs'
   | 'pluginOptions.plugins.ssrAPIs'
   | 'pluginOptions.plugins.pluginFilepath'
-  | 'pluginOptions.gtagConfig.anonymize_ip'
-  | 'pluginOptions.gtagConfig.cookie_expires'
-  | 'pluginOptions.pluginConfig.head'
-  | 'pluginOptions.pluginConfig.respectDNT'
-  | 'pluginOptions.trackingIds'
   | 'pluginOptions.outputPath'
   | 'pluginOptions.showSpinner'
   | 'pluginOptions.name'
@@ -3233,6 +3228,11 @@ type SitePluginFieldsEnum =
   | 'pluginOptions.allExtensions'
   | 'pluginOptions.isTSX'
   | 'pluginOptions.jsxPragma'
+  | 'pluginOptions.gtagConfig.anonymize_ip'
+  | 'pluginOptions.gtagConfig.cookie_expires'
+  | 'pluginOptions.pluginConfig.head'
+  | 'pluginOptions.pluginConfig.respectDNT'
+  | 'pluginOptions.trackingIds'
   | 'nodeAPIs'
   | 'browserAPIs'
   | 'ssrAPIs'
@@ -3346,9 +3346,6 @@ type SitePluginPackageJsonPeerDependenciesFilterListInput = {
 
 type SitePluginPluginOptions = {
   readonly plugins: Maybe<ReadonlyArray<Maybe<SitePluginPluginOptionsPlugins>>>;
-  readonly gtagConfig: Maybe<SitePluginPluginOptionsGtagConfig>;
-  readonly pluginConfig: Maybe<SitePluginPluginOptionsPluginConfig>;
-  readonly trackingIds: Maybe<ReadonlyArray<Maybe<Scalars['String']>>>;
   readonly outputPath: Maybe<Scalars['String']>;
   readonly showSpinner: Maybe<Scalars['Boolean']>;
   readonly name: Maybe<Scalars['String']>;
@@ -3383,6 +3380,9 @@ type SitePluginPluginOptions = {
   readonly allExtensions: Maybe<Scalars['Boolean']>;
   readonly isTSX: Maybe<Scalars['Boolean']>;
   readonly jsxPragma: Maybe<Scalars['String']>;
+  readonly gtagConfig: Maybe<SitePluginPluginOptionsGtagConfig>;
+  readonly pluginConfig: Maybe<SitePluginPluginOptionsPluginConfig>;
+  readonly trackingIds: Maybe<ReadonlyArray<Maybe<Scalars['String']>>>;
 };
 
 type SitePluginPluginOptionsFeeds = {
@@ -3403,9 +3403,6 @@ type SitePluginPluginOptionsFeedsFilterListInput = {
 
 type SitePluginPluginOptionsFilterInput = {
   readonly plugins: Maybe<SitePluginPluginOptionsPluginsFilterListInput>;
-  readonly gtagConfig: Maybe<SitePluginPluginOptionsGtagConfigFilterInput>;
-  readonly pluginConfig: Maybe<SitePluginPluginOptionsPluginConfigFilterInput>;
-  readonly trackingIds: Maybe<StringQueryOperatorInput>;
   readonly outputPath: Maybe<StringQueryOperatorInput>;
   readonly showSpinner: Maybe<BooleanQueryOperatorInput>;
   readonly name: Maybe<StringQueryOperatorInput>;
@@ -3440,6 +3437,9 @@ type SitePluginPluginOptionsFilterInput = {
   readonly allExtensions: Maybe<BooleanQueryOperatorInput>;
   readonly isTSX: Maybe<BooleanQueryOperatorInput>;
   readonly jsxPragma: Maybe<StringQueryOperatorInput>;
+  readonly gtagConfig: Maybe<SitePluginPluginOptionsGtagConfigFilterInput>;
+  readonly pluginConfig: Maybe<SitePluginPluginOptionsPluginConfigFilterInput>;
+  readonly trackingIds: Maybe<StringQueryOperatorInput>;
 };
 
 type SitePluginPluginOptionsGtagConfig = {
@@ -3598,6 +3598,19 @@ type BlogListQuery = { readonly allMarkdownRemark: { readonly edges: ReadonlyArr
         & { readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title' | 'createdAt' | 'updatedAt' | 'description' | 'category' | 'tags'>> }
       ) }> } };
 
+type LayoutQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+type LayoutQuery = { readonly site: Maybe<{ readonly siteMetadata: Maybe<(
+      Pick<SiteSiteMetadata, 'title'>
+      & { readonly social: Maybe<Pick<SiteSiteMetadataSocial, 'gitHub'>> }
+    )> }> };
+
+type PagesQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+type PagesQuery = { readonly posts: { readonly edges: ReadonlyArray<{ readonly next: Maybe<{ readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title'>>, readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>> }>, readonly previous: Maybe<{ readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title'>> }>, readonly node: { readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title'>> } }>, readonly tags: ReadonlyArray<Pick<MarkdownRemarkGroupConnection, 'fieldValue'>> }, readonly about: Maybe<{ readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'tags' | 'title'>> }>, readonly privacyPolicy: Maybe<{ readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title'>> }> };
+
 type SeoQueryVariables = Exact<{ [key: string]: never; }>;
 
 
@@ -3606,10 +3619,13 @@ type SeoQuery = { readonly site: Maybe<{ readonly siteMetadata: Maybe<(
       & { readonly social: Maybe<Pick<SiteSiteMetadataSocial, 'twitter'>> }
     )> }> };
 
-type PagesQueryVariables = Exact<{ [key: string]: never; }>;
+type RssQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-type PagesQuery = { readonly posts: { readonly edges: ReadonlyArray<{ readonly next: Maybe<{ readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title'>>, readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>> }>, readonly previous: Maybe<{ readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title'>> }>, readonly node: { readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title'>> } }>, readonly tags: ReadonlyArray<Pick<MarkdownRemarkGroupConnection, 'fieldValue'>> }, readonly about: Maybe<{ readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'tags' | 'title'>> }>, readonly privacyPolicy: Maybe<{ readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title'>> }> };
+type RssQuery = { readonly allMarkdownRemark: { readonly edges: ReadonlyArray<{ readonly node: (
+        Pick<MarkdownRemark, 'excerpt' | 'html'>
+        & { readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title' | 'updatedAt' | 'createdAt' | 'description'>> }
+      ) }> } };
 
 type AboutBySlugQueryVariables = Exact<{
   slug: Scalars['String'];
@@ -3620,6 +3636,14 @@ type AboutBySlugQuery = { readonly site: Maybe<{ readonly siteMetadata: Maybe<Pi
     Pick<MarkdownRemark, 'id' | 'excerpt' | 'html' | 'tableOfContents'>
     & { readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title' | 'createdAt' | 'updatedAt' | 'description' | 'tags' | 'userId'>>, readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>> }
   )> };
+
+type RssSiteQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+type RssSiteQuery = { readonly site: Maybe<{ readonly siteMetadata: Maybe<(
+      Pick<SiteSiteMetadata, 'title' | 'description' | 'siteUrl'>
+      & { site_url: SiteSiteMetadata['siteUrl'] }
+    )> }> };
 
 type GatsbyImageSharpFixedFragment = Pick<ImageSharpFixed, 'base64' | 'width' | 'height' | 'src' | 'srcSet'>;
 
@@ -3671,13 +3695,15 @@ type GatsbyImageSharpSizes_noBase64Fragment = Pick<ImageSharpSizes, 'aspectRatio
 
 type GatsbyImageSharpSizes_withWebp_noBase64Fragment = Pick<ImageSharpSizes, 'aspectRatio' | 'src' | 'srcSet' | 'srcWebp' | 'srcSetWebp' | 'sizes'>;
 
-type RssQueryVariables = Exact<{ [key: string]: never; }>;
+type BlogPostBySlugQueryVariables = Exact<{
+  slug: Scalars['String'];
+}>;
 
 
-type RssQuery = { readonly allMarkdownRemark: { readonly edges: ReadonlyArray<{ readonly node: (
-        Pick<MarkdownRemark, 'excerpt' | 'html'>
-        & { readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title' | 'updatedAt' | 'createdAt' | 'description'>> }
-      ) }> } };
+type BlogPostBySlugQuery = { readonly site: Maybe<{ readonly siteMetadata: Maybe<Pick<SiteSiteMetadata, 'title' | 'siteUrl'>> }>, readonly markdownRemark: Maybe<(
+    Pick<MarkdownRemark, 'id' | 'excerpt' | 'html' | 'tableOfContents'>
+    & { readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title' | 'createdAt' | 'updatedAt' | 'description' | 'tags' | 'userId'>>, readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>> }
+  )> };
 
 type PrivacyPolicyBySlugQueryVariables = Exact<{
   slug: Scalars['String'];
@@ -3689,22 +3715,6 @@ type PrivacyPolicyBySlugQuery = { readonly site: Maybe<{ readonly siteMetadata: 
     & { readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title' | 'createdAt' | 'updatedAt' | 'description' | 'tags' | 'userId'>> }
   )> };
 
-type RssSiteQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-type RssSiteQuery = { readonly site: Maybe<{ readonly siteMetadata: Maybe<(
-      Pick<SiteSiteMetadata, 'title' | 'description' | 'siteUrl'>
-      & { site_url: SiteSiteMetadata['siteUrl'] }
-    )> }> };
-
-type LayoutQueryVariables = Exact<{ [key: string]: never; }>;
-
-
-type LayoutQuery = { readonly site: Maybe<{ readonly siteMetadata: Maybe<(
-      Pick<SiteSiteMetadata, 'title'>
-      & { readonly social: Maybe<Pick<SiteSiteMetadataSocial, 'gitHub'>> }
-    )> }> };
-
 type TagByTagQueryVariables = Exact<{
   tag: Scalars['String'];
 }>;
@@ -3714,15 +3724,5 @@ type TagByTagQuery = { readonly allMarkdownRemark: { readonly edges: ReadonlyArr
         Pick<MarkdownRemark, 'excerpt'>
         & { readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>>, readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title' | 'createdAt' | 'updatedAt' | 'description' | 'category' | 'tags'>> }
       ) }> } };
-
-type BlogPostBySlugQueryVariables = Exact<{
-  slug: Scalars['String'];
-}>;
-
-
-type BlogPostBySlugQuery = { readonly site: Maybe<{ readonly siteMetadata: Maybe<Pick<SiteSiteMetadata, 'title' | 'siteUrl'>> }>, readonly markdownRemark: Maybe<(
-    Pick<MarkdownRemark, 'id' | 'excerpt' | 'html' | 'tableOfContents'>
-    & { readonly frontmatter: Maybe<Pick<MarkdownRemarkFrontmatter, 'title' | 'createdAt' | 'updatedAt' | 'description' | 'tags' | 'userId'>>, readonly fields: Maybe<Pick<MarkdownRemarkFields, 'slug'>> }
-  )> };
 
 }
